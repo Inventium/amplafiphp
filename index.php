247b662bf2f3b933c8b496fccb46d4338b936827
Dave Doolin <david.doolin@gmail.com>
2010-12-20 13:32:59 -0800

    Fixing a merge.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<h1>Test code for Amplafi PHP client</h1>
+
+<?php
+
+//$fp = fsockopen("www.example.com", 80, $errno, $errstr, 30);
+$fp = fsockopen("amplafi.net", 80, $errno, $errstr, 30);
+
+
+if (!$fp) {
+    echo "$errstr ($errno)<br />\n";
+} else {
+    $out = "GET / HTTP/1.1\r\n";
+    $out .= "Host: www.example.com\r\n";
+    $out .= "Connection: Close\r\n\r\n";
+    fwrite($fp, $out);
+    while (!feof($fp)) {
+        echo fgets($fp, 128);
+    }
+    fclose($fp);
+}
+?>
+
