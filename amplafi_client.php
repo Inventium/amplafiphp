8f8d600082f5b80e8f7a498ac9bcc8d2d50b3020
Patrick Moore <patmoore@amplafi.com>
2010-12-20 14:59:09 -0800

    more refactoring for understanding

--- amplafi_client.php
+++ amplafi_client.php
@@ -23,26 +23,25 @@ class AmplafiApiClient {
     var $errors = array();
 
     function AmplafiApiClient( ) {
-        $this->partnerGUID = $partnerGUID;
-        $this->userCode = $userCode;
     }
 
     function send_request() {
-        $this->request_xml  = "";
-
+        var response_json = '';
+        var request_json = '';
+        
         if ( function_exists( 'wp_remote_post' ) ) {
             $response = wp_remote_post( $this->amplafi_uri, array(
                 'headers' => array( 'Content-Type' => $this->content_type, 'Content-Length' => strlen( $this->request ) ),
-                'user-agent' => $this->version,
+                'user-agent' => $this->user_agent,
                 'body' => $this->request
             ) );
             if ( !$response || is_wp_error( $response ) ) {
                 $errors[-1] = "Can't connect";
                 return false;
             }
-            $this->response_xml = wp_remote_retrieve_body( $response );
+            $response_json = wp_remote_retrieve_body( $response );
         } else {
-            $parsed = parse_url( $this->polldaddy_url );
+            $parsed = parse_url( $this->amplafi_url );
 
             if ( !isset( $parsed['host'] ) && !isset( $parsed['scheme'] ) ) {
                 $errors[-1] = 'Invalid API URL';
@@ -61,49 +60,39 @@ class AmplafiApiClient {
                 return false;
             }
 
-            if ( function_exists( 'stream_set_timeout' ) )
+            if ( function_exists( 'stream_set_timeout' ) ){
                 stream_set_timeout( $fp, 3 );
-
-            if ( !isset( $parsed['path']) || !$path = $parsed['path'] . ( isset($parsed['query']) ? '?' . $parsed['query'] : '' ) )
+            }
+            if ( !isset( $parsed['path']) || !$path = $parsed['path'] . ( isset($parsed['query']) ? '?' . $parsed['query'] : '' ) ) {
                 $path = '/';
-
+            }
+            
             $request  = "POST $path HTTP/1.0\r\n";
             $request .= "Host: {$parsed['host']}\r\n";
             $request .= "User-agent: ". $this->user_agent . "\r\n";
             $request .= "Content-Type: ". $content_type . "\r\n";
-            $request .= 'Content-Length: ' . strlen( $this->request_xml ) . "\r\n";
+            $request .= 'Content-Length: ' . strlen( $this->request_json ) . "\r\n";
 
-            fwrite( $fp, "$request\r\n$this->request_xml" );
+            fwrite( $fp, "$request\r\n$this->request" );
 
             $response = '';
             while ( !feof( $fp ) )
                 $response .= fread( $fp, 4096 );
             fclose( $fp );
 
-
             if ( !$response ) {
                 $errors[-2] = 'No Data';
             }
 
-            list($headers, $this->response_xml) = explode( "\r\n\r\n", $response, 2 );
+            list($headers, $this->response_json) = explode( "\r\n\r\n", $response, 2 );
         }
 
-        $this->responses[] = $this->response_xml;
+        $this->responses[] = $response_json;
 
-        $parser = new PollDaddy_XML_Parser( $this->response_xml );
-        $this->response =& $parser->objects[0];
-        if ( isset( $this->response->errors->error ) ) {
-            if ( !is_array( $this->response->errors->error ) )
-                $this->response->errors->error = array( $this->response->errors->error );
-            foreach ( $this->response->errors->error as $error )
-                $this->errors[$error->_id] = $error->___content;
-        }
     }
     function reset() {
         $this->request = null;
         $this->response = null;
-        $this->request_xml = '';
-        $this->response_xml = '';
         $this->errors = array();
     }
 
