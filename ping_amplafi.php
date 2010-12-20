91b2d83976952765d05f9490e8f7c56ced4932dc
Dave Doolin <david.doolin@gmail.com>
2010-12-20 13:05:29 -0800

    Moving dev to localhost.

--- ping_amplafi.php
+++ ping_amplafi.php
@@ -1,5 +1,28 @@
 #/usr/bin/php
 
+#<?php
+#class api_client {
+     
+   
+$hostname = 'http://amplafi.net/apiv1/';
 
-echo "hi\n";
-printf "hi\n";
+/*
+$fp = fsockopen(
+        $parsed['host'],
+        $parsed['scheme'] == 'ssl' || $parsed['scheme'] == 'https' && extension_loaded('openssl') ? 443 : 80,
+        $err_num,
+        $err_str,
+        3
+      );
+*/
+$fp = fsockopen($hostame,80);
+
+
+      if ( !$fp ) {
+        $errors[-1] = "Can't connect";
+        return false;
+      }
+
+
+#?>
+
