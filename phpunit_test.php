fc87f90b6e0027f316482075f8f7353c5d568b0a
Dave Doolin <david.doolin@gmail.com>
2010-12-20 19:38:15 -0800

    Working unit test demo case.

--- phpunit_test.php
+++ phpunit_test.php
@@ -5,6 +5,17 @@ require_once('PHPUnit/Autoload.php');
 
 class DataTest extends PHPUnit_Framework_TestCase {
 
+  function test() {
+    $stack = array();
+    $this->assertEquals(0, count($stack));
+    // This will throw an exception
+    //$this->assertEquals(1, count($stack));
+  }
 }
+
+$dt = new DataTest();
+
+// Runs silently from command line:
+$dt->test();
 
 ?>
